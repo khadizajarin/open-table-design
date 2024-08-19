45909f4be81ff63000e1e1cf0154b6047e73038a
khadizajarin <khadiza131310@gmail.com>
2024-08-19 21:54:23 +0600

    adding card

---
+++ src/app/CommonComps/Service.js
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import { Cinzel,Ubuntu } from 'next/font/google';
+import cardFinisher from '@/assets/CardFinisher-01.png'
+const cinzel = Cinzel({
+    subsets: ['latin'],
+    weight: ['400', '700','900'], 
+  });
+
+const ubuntu = Ubuntu({
+    subsets: ['latin'],
+    weight: ['400', '500','700'], 
+  });
+
+const Service = ({image,cardTitle, text}) => {
+    return (
+        <div className='z-10 bg-[#042423] w-[78rem] shadow-black shadow-[2px_-2px_10px_rgba(0,0,0,0.2),10px_10px_15px_rgba(0,0,0,0.2)]'>
+            <Image
+            src={image}
+            alt={image}
+            ></Image>
+
+            <div className='h-[25rem]'>
+                <h1 className='pb-[1.7rem] pt-[3rem] text-[#BCA263] text-[2rem] text-center'
+                style ={{fontFamily: cinzel.style.fontFamily, fontSize: '2.7rem', fontWeight: '700' }}>{cardTitle}</h1>
+
+                <div className='flex justify-center items-center'>
+                    <hr className='border-[#BCA263] w-[12rem] pb-[2.375rem]'/>
+                </div>
+
+                <p className='pb-[3rem] text-[#BCA263]  text-center px-[11.25rem] tracking-wider leading-loose'
+                style ={{fontFamily: ubuntu.style.fontFamily, fontSize: '1.1rem', fontWeight: '400'}}>{text}</p>
+
+                <div className='flex justify-center items-center'>
+                    <Image className='w-[16rem]' src={cardFinisher} alt={cardFinisher}></Image>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/app/Services.js
+++ src/app/Services.js
@@ -1,22 +1,35 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Cinzel, Ubuntu } from 'next/font/google';
+import { Cinzel,Ubuntu } from 'next/font/google';
+import Image from 'next/image';
 import Heading from './CommonComps/Heading';
+import shadowImage from '@/assets/Card Background-01.png';
+import Service from './CommonComps/Service';
+import card1 from '@/assets/C1-01.png';
+import card2 from '@/assets/C2-01.png';
+import card3 from '@/assets/C3-01.png';
+import card4 from '@/assets/C4-01.png';
 
-// Import the Cinzel font
 const cinzel = Cinzel({
     subsets: ['latin'],
-    weight: ['400', '700','900'], // Specify the weights you need
+    weight: ['400', '700','900'], 
   });
-// Import the Ubuntu font
+
 const ubuntu = Ubuntu({
     subsets: ['latin'],
-    weight: ['400', '500','700'], // Specify the weights you need
+    weight: ['400', '500','700'], 
   });
   
 
 const Services = () => {
     return (
-        <div className='bg-[#0B1519] flex justify-center items-center flex-col'>
+        <div className='bg-[#0B1519] h-full flex justify-center items-center flex-col relative'>
+            <div className='absolute top-0 left-0 w-full h-full '>
+                <Image 
+                    src={shadowImage}
+                    className=''
+                    alt="Shadow"
+                />
+            </div>
             <div className='pt-[5.75rem] mb-[4.375rem]'>
                 <Heading  text={'ABOUT OUR SERVICES'}></Heading>
             </div>
@@ -28,6 +41,40 @@ const Services = () => {
                 survived notonly five centuries, but also the leap into electronic typesetting, <br/>
                 remaining essentially unchanged.
             </p>
+
+            <div className='flex flex-col gap-[8rem]'>
+            <Service image={card1} cardTitle={`THE DINING ROOM (restaurant)`}
+            text={ <>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/>
+                Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown<br/>
+                printer took a galley of type and scrambled it to make a type specimen book.
+            </>}></Service>
+
+            <Service image={card2} cardTitle={`ALL RISE (bakery))`}
+            text={ <>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/>
+            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown<br/>
+            printer took a galley of type and scrambled it to make a type specimen book.
+            </>}>
+            </Service>
+
+            <Service image={card3} cardTitle={`GATHER YOUR PEERS (live music & bar)`}
+            text={ <>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/>
+            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown<br/>
+            printer took a galley of type and scrambled it to make a type specimen book.
+            </>}>
+            </Service>
+
+            <Service image={card4} cardTitle={`HOLD COURT (private Dinning)`}
+            text={ <>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem <br/>
+            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown<br/>
+            printer took a galley of type and scrambled it to make a type specimen book.
+            </>}>
+            </Service>
+            </div>
+
             
         </div>
     );
